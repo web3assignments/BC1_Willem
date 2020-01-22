3e6a17b530e999434bf42ab4228f54366ac42b2d
willemvandergaag <37209583+willemvandergaag@users.noreply.github.com>
2020-01-22 11:04:40 +0100

    Create WhatAreTheOdds with natpsec.sol

---
+++ Day_12_Wed_8_1_2020/WhatAreTheOdds with natpsec.sol
@@ -0,0 +1,97 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+/// @title What are the odds?
+/// @author Willem vand der Gaag
+/// @notice You can use this contract for only the most basic simulation
+/// @dev All function calls are currently implemented without side effects
+
+contract WhatAreTheOdds{
+    
+/// @author Willem van der Gaag
+/// @notice Player 1 first has to give the odds, with a maximum of int1
+/// @notice Player 1 has to guess a number between 0 and the odds
+/// @notice Player 2 has to guess a number between 0 and the odds
+/// @notice If Player 1 and 2 both guess the same number, Player 1 wins
+/// @notice Otherwise Player 2 wins
+/// @dev this is the application in it's most basic form
+/// @param odds, int between 0 and int
+/// @param guessOne, int between 0 and odds
+/// @param guessTwo, int between 0 and odds
+/// @param winner, int of player that has Won
+/// @param winsPLayerOne, number of wins player 1
+/// @param winsPLayerTwo, number of wins player 2
+/// @return GuessNumberTwo, winner of the game
+/// @return List1Length, number of guesses player 1
+/// @return List2Length , number of guesses player 2
+
+    event Log(string message, uint guess);   // declaring event  
+  	uint public odds;
+	uint public guessOne;
+  	uint public guessTwo;
+  	uint public winner;
+  	uint public winsPlayerOne;
+  	uint public winsPlayerTwo;
+    constructor() public payable {}
+  	
+    struct member {
+        uint data;
+    }
+    
+    member[] public GuessedOddsPlayer1;
+    member[] public GuessedOddsPlayer2;
+  
+    function GiveTheOddsPlayerOne(uint GiveOdds) public  { 
+        odds=GiveOdds;
+        require(odds < 11, "Odds too high");
+        require(odds > 1, "odds have to be above 2");
+        emit Log("The odds are", odds);
+    }
+  	
+  	function GuessNumberOne(uint FirstOdds) public {
+  	    uint list1 = List1Length();
+        uint list2 = List2Length();
+        require(list1 == list2, "Not your turn");
+       	guessOne =FirstOdds;
+      	require(odds > 0, "No odds given");
+      	require(guessOne <= odds,"Has to be lower than the odd");
+        member memory temp;
+        temp.data=FirstOdds;
+        GuessedOddsPlayer1.push(temp);
+
+      	emit Log("Player one guessed: ", guessOne);
+    }
+    
+    function GuessNumberTwo(uint SecondOdds) public returns(uint) {
+        uint list1 = List1Length();
+        uint list2 = List2Length();
+        require(list1 == list2 + 1, "Not your turn");
+        guessTwo =SecondOdds;
+        require(odds > 0, "No odds given");
+      	require(guessTwo <= odds,"Has to be lower than the odd");
+      	member memory temp;
+        temp.data=SecondOdds;
+        GuessedOddsPlayer2.push(temp);
+      	emit Log("Player two guessed: ", guessTwo);
+      	
+      	if(guessTwo == guessOne){
+      	    winner = 1;
+            winsPlayerOne++;
+            emit Log("Won by player: ", winner);
+        }
+      	else{
+            winner = 2;
+            winsPlayerTwo++;
+            emit Log("Won by player: ", winner);
+        }
+        return winner;
+    }
+    
+    function List1Length() public view returns(uint) {
+      return GuessedOddsPlayer1.length;
+    }
+    
+    function List2Length() public view returns(uint) {
+      return GuessedOddsPlayer2.length;
+    }
+    
+}
